c223ea6b011cfd6226c91c106e4f0a6dca36d170
Dhruv Aggarwal <51310468+dhruv8920@users.noreply.github.com>
2021-07-04 22:17:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,65 +1,53 @@
-from flask import Flask, render_template,request
-from datetime import date
-import numpy as np
+from flask import Flask, render_template, request
 import pickle
+import numpy as np
+from datetime import date
 
-model = pickle.load(open('car_price_regression_model.pkl', 'rb'))
 app = Flask(__name__)
+model = pickle.load(open('car_price_regression_model.pkl', 'rb'))
 
 
 @app.route('/')
-def home():
-    render_template('index.html')
+def Home():
+    return render_template('test.html')
 
 
-@app.route('/predict', method = ['POST'])
+@app.route("/predict", methods=['POST'])
 def predict():
     if request.method == 'POST':
-
-        year = int(request.form['Year'])
-        cur_date = date.today()
-        year = cur_date.year - year
-        present_price = float(request.form['Present_Price'])
-        kms_driven = int(request.form['Kms_Driven'])
-        owner = int(request.form['Owner'])
-        fuel_type_petrol = request.form['Fuel_Type_Petrol']
-
-        if fuel_type_petrol == 'Petrol':
-            fuel_type_petrol = 1
-            fuel_type_diesel = 0
-
-        elif fuel_type_petrol == 'Diesel':
-            fuel_type_diesel = 1
-            fuel_type_petrol = 0
+        Year = int(request.form['Year'])
+        Present_Price = float(request.form['Present_Price'])
+        Kms_Driven = int(request.form['Kms_Driven'])
+        Owner = int(request.form['Owner'])
+        Fuel_Type_Petrol = request.form['Fuel_Type_Petrol']
+        if(Fuel_Type_Petrol == 'Petrol'):
+                Fuel_Type_Petrol = 1
+                Fuel_Type_Diesel = 0
 
         else:
-            fuel_type_diesel = 0
-            fuel_type_petrol = 0
+            Fuel_Type_Petrol = 0
+            Fuel_Type_Diesel = 1
 
-        seller_type_individual = request.form['Seller_Type_Individual']
-        if seller_type_individual == 'Individual':
-            seller_type_individual = 1
-
-        else:
-            seller_type_individual = 0
-
-        transmission_manual = request.form['Transmission_Manual']
-        if transmission_manual == 'Manual':
-            transmission_manual = 1
+        Year = date.today().year -Year
+        Seller_Type_Individual = request.form['Seller_Type_Individual']
+        if(Seller_Type_Individual == 'Individual'):
+            Seller_Type_Individual = 1
 
         else:
-            transmission_manual = 0
+            Seller_Type_Individual = 0
 
-        data = np.array([['present_price','kms_driven','owner','year','fuel_type_diesel','fuel_type_petrol','seller_type_individual','transmission_manual']])
-        prediction = model.predict(data)
+        Transmission_Manual = request.form['Transmission_Manual']
+        if(Transmission_Manual == 'Manual'):
+            Transmission_Manual = 1
+        else:
+            Transmission_Manual = 0
+        prediction = model.predict([[Present_Price,Kms_Driven,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Manual]])
         price = round(prediction[0],2)
-
-        if price > 0 :
-            render_template('index.html',text = "You Cannot Sell this Car.")
-
+        if price<0:
+            return render_template('test.html',message="Sorry you cannot sell this car")
         else:
-            render_template('index.html',text ="The Estimated Price is {}".format(price))
+            return render_template('test.html',message="You Can Sell The Car at {} Lacs".format(price))
 
-
-if __name__ == "__main__":
-    app.run(debug = True)
+if __name__=="__main__":
+    app.run(debug=True)
+          
